21ac779c6b6c91c1dcff78bb17c8d6325e23c555
root <819046834>
2019-03-12 06:42:40 +0800

    test

--- gitpull.sh
+++ gitpull.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-git config --global user.email "wangminxin6@126.com"
+git config --global user.email "819046834@qq.com"
 
-git config --global user.name "minxin126"
+git config --global user.name "819046834"
 
-#git clone https://github.com/minxin126/ubuntu-linux.git
+#git clone https://github.com/819046834/ubuntu-linux.git
 #git add .
 #git commit -a -m ""
 sudo git pull origin master
